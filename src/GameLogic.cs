acdfeb5619c239462bceeb90ab3b905bd281d6e6
vickyyyy <100065232@students.swinburne.edu.my>
2017-10-02 11:32:13 +0800

    Add files via upload
    Pushing files.

---
+++ src/GameLogic.cs
@@ -0,0 +1,38 @@
+
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+/// <summary>
+/// The game logic is where the game is loaded.
+/// </summary>
+
+static class GameLogic
+{
+	public static void Main()
+	{
+		//Opens a new Graphics Window
+		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+
+		//Load Resources
+		LoadResources();
+
+		SwinGame.PlayMusic(GameMusic("Background"));
+
+		//Game Loop
+		do {
+			HandleUserInput();
+			DrawScreen();
+		} while (!(SwinGame.WindowCloseRequested() == true | CurrentState == GameState.Quitting));
+
+		SwinGame.StopMusic();
+
+		//Free Resources and Close Audio, to end the program.
+		FreeResources();
+	}
+}
+
+
