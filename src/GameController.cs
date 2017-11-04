42304a27a3ef3ceee215df6ff552fcc1580d7149
prioprio34 <prioprio34@gmail.com>
2017-11-04 23:12:56 +0800

    changes

--- src/GameController.cs
+++ src/GameController.cs
@@ -82,6 +82,9 @@ namespace MyGame
 
 			//create the players
 			switch (_aiSetting) {
+				case AIOption.Easy:
+				_ai = new AIEasyPlayer (_theGame);
+				break;
 			case AIOption.Medium:
 				_ai = new AIMediumPlayer (_theGame);
 				break;
@@ -89,7 +92,7 @@ namespace MyGame
 				_ai = new AIHardPlayer (_theGame);
 				break;
 			default:
-				_ai = new AIMediumPlayer (_theGame);
+				_ai = new AIEasyPlayer (_theGame);
 				break;
 			}
 
@@ -113,6 +116,9 @@ namespace MyGame
             //create the players
             switch (_aiSetting)
             {
+				case AIOption.Easy:
+				_ai = new AIEasyPlayer (_theGame);
+				break;
                 case AIOption.Medium:
                     _ai = new AIMediumPlayer(_theGame);
                     break;
@@ -120,7 +126,7 @@ namespace MyGame
                     _ai = new AIHardPlayer(_theGame);
                     break;
                 default:
-                    _ai = new AIMediumPlayer(_theGame);
+                    _ai = new AIEasyPlayer(_theGame);
                     break;
             }
 
--- src/MenuController.cs
+++ src/MenuController.cs
@@ -335,10 +335,10 @@ namespace MyGame
 		{
 			switch (button) {
 			case SETUP_MENU_EASY_BUTTON:
-				GameController.SetDifficulty (AIOption.Hard);
+				GameController.SetDifficulty (AIOption.Easy);
 				break;
 			case SETUP_MENU_MEDIUM_BUTTON:
-				GameController.SetDifficulty (AIOption.Hard);
+				GameController.SetDifficulty (AIOption.Medium);
 				break;
 			case SETUP_MENU_HARD_BUTTON:
 				GameController.SetDifficulty (AIOption.Hard);
